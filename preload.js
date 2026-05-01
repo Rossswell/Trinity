768080734536c473c903830415377866e59cb0ea
Rossswell <haroldcaballero22@gmail.com>
2026-05-01 14:23:24 -0400

    v1.0.9: Panel Admin, theme #1b1b1f, sidebar alignment, search icon

--- preload.js
+++ preload.js
@@ -4,17 +4,18 @@ contextBridge.exposeInMainWorld('api', {
     onUpdateMessage:    (cb) => ipcRenderer.on('update-message',    (_e, m) => cb(m)),
     onUpdateProgress:   (cb) => ipcRenderer.on('update-progress',   (_e, p) => cb(p)),
     onUpdateDownloaded: (cb) => ipcRenderer.on('update-downloaded',  (_e, m) => cb(m)),
+    getEnvConfig:       ()  => ipcRenderer.invoke('get-env-config'),
 
-    // Stalker
-    getStalkerData:     ()        => ipcRenderer.invoke('stalker:get-data'),
-    analyzeProduct:     (name)    => ipcRenderer.invoke('stalker:analyze-product', name),
+    // Stalker (sector: 'mmx' | 'am')
+    getStalkerData:     (sector)  => ipcRenderer.invoke('stalker:get-data', sector),
+    analyzeProduct:     (name, sector) => ipcRenderer.invoke('stalker:analyze-product', name, sector),
     stopAnalysis:       ()        => ipcRenderer.invoke('stalker:stop'),
     openMlLogin:        ()        => ipcRenderer.invoke('stalker:open-ml-login'),
     onMlLoginDone:      (cb) => ipcRenderer.on('stalker:ml-login-done', (_e) => cb()),
-    getLinksData:       ()        => ipcRenderer.invoke('stalker:get-links'),
+    getLinksData:       (sector)  => ipcRenderer.invoke('stalker:get-links', sector),
     updateLinkRow:      (data)    => ipcRenderer.invoke('stalker:update-link-row', data),
     insertLinks:        (data)    => ipcRenderer.invoke('stalker:insert-links', data),
-    syncAllLinks:       ()        => ipcRenderer.invoke('stalker:sync-all-links'),
+    syncAllLinks:       (sector)  => ipcRenderer.invoke('stalker:sync-all-links', sector),
     checkForUpdates:    ()        => ipcRenderer.invoke('check-for-updates'),
     getVersion:         ()        => ipcRenderer.invoke('get-version'),
 
@@ -36,4 +37,16 @@ contextBridge.exposeInMainWorld('api', {
     onStalkerProgress:  (cb) => ipcRenderer.on('stalker:progress',   (_e, d) => cb(d)),
     onStalkerLinkDone:  (cb) => ipcRenderer.on('stalker:link-done',  (_e, d) => cb(d)),
     onStalkerLinkError: (cb) => ipcRenderer.on('stalker:link-error', (_e, d) => cb(d)),
+    
+    // Registros
+    registroAdd: (data) => ipcRenderer.invoke('registro:add', data),
+    registroGetHistory: (data) => ipcRenderer.invoke('registro:get-history', data),
+
+    // Ventas (POS)
+    ventaAdd: (data) => ipcRenderer.invoke('venta:add', data),
+    ventaGetHistory: () => ipcRenderer.invoke('venta:get-history'),
+    clientesGet: () => ipcRenderer.invoke('clientes:get'),
+    clientesAdd: (data) => ipcRenderer.invoke('clientes:add', data),
+    clientesUpdate: (data) => ipcRenderer.invoke('clientes:update', data),
+    imprimirFactura: (data) => ipcRenderer.invoke('factura:print-pdf', data),
 });
