bd1e32293c0e4164f64a24f2fafbd3bb30070922
Rossswell <haroldcaballero22@gmail.com>
2026-05-01 14:33:35 -0400

    security: remove scratch.js and test.js with exposed API keys from tracking

--- scratch.js
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-// Very simple script to read the image file and get a hex code. We don't have an image parser built-in easily for node, 
-// wait, we can just ask the user or guess from typical dark themes. 
--- test.js
+++
@@ -1,6 +0,0 @@
-// Node 18+ has fetch global
-fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=AIzaSyCDvqxhWqHl5j8g22c2lKjI1CO-t3HF3gc", {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ contents: [{ parts: [{ text: "Hi" }]}] })
-}).then(res => res.json()).then(console.log).catch(console.error);
